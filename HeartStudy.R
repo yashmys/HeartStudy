50a752b5ca60a11462d23319289753c206fd593c
yash <yash.mys@gmail.com>
2018-06-02 01:06:52 -0400

    commit

--- HeartStudy.R
+++ HeartStudy.R
@@ -1,7 +1,8 @@
 install.packages('caTools')
 library('caTools')
 
-
+install.packages('ROCR')
+library('ROCR')
 
 quality <- read.csv("quality.csv")
 quality
@@ -11,6 +12,30 @@ str(quality)
 table(quality$PoorCare)
 
 set.seed(88)
-split <-  sample.split()
+split <-  sample.split(quality$PoorCare,SplitRatio = 0.75)
 
-??sample.s
+qualityTrain <-  subset(quality,split == TRUE)
+qualityTest <-  subset(quality,split == FALSE )
+
+nrow(qualityTest)
+nrow(qualityTrain)
+
+
+QualityLog <-  glm(qualityTrain$PoorCare ~ qualityTrain$OfficeVisits + qualityTrain$Narcotics, data = qualityTrain , family=binomial() )
+summary(QualityLog)
+
+predictTrain <- predict(QualityLog,type = "response")
+summary(predictTrain)
+tapply(predictTrain,qualityTrain$PoorCare,mean)
+
+table(qualityTrain$PoorCare,predictTrain > 0.5)
+table(qualityTrain$PoorCare,predictTrain > 0.7)
+table(qualityTrain$PoorCare,predictTrain > 0.2)
+
+ROCRpred <-  prediction(predictTrain,qualityTrain$PoorCare)
+class(ROCRpred)
+ROCRpred
+ROCRperf <- performance(ROCRpred,"tpr","fpr")
+class(ROCRperf)
+ROCRperf
+plot(ROCRperf,colorize =TRUE, print.cutoffs.at=seq(0,1,0.1),text.adj= c(-0.2,1.7))
